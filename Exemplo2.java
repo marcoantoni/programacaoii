3d309341428da61552f47ebab668820607847f32
Marco Antoni <marco.antoni910@gmail.com>
2023-03-21 08:49:37 -0300

    Testes condicionais e inputs

--- Exemplo2.java
+++ Exemplo2.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;	// importa a biblioteca para ler do teclado
+
 class Exemplo2 {
 	
 	public static void main (String args[]) {
 		
+		// cria a variavel que permitirá fazer o input
+		Scanner sc = new Scanner(System.in);	
+		
+		System.out.printf("Qual seu nome?\n");
+		String nome = sc.nextLine();
+		
 		boolean ehEstudante = false;
-		float saldo = 202.93f;
+		System.out.printf("Qual é o seu saldo?\n");
+		float saldo = sc.nextFloat();	//sc.nextFloat(); vai ler um número com ponto
 		
 		// regra para não pagar tarifa bancária: ser estudante OU ter saldo igual ou superior de 2 mil
 		if (ehEstudante == true || saldo >= 2000) {
@@ -12,17 +21,20 @@ class Exemplo2 {
 			System.out.printf("Deve pagar tarifa para manutenção de conta\n");
 		}
 		
+		
+		
 		// condições para tirar CNH:
 		// maioridade E também passar no teste psicológico
-		int idade = 17;
+		System.out.printf("Qual sua idade? \n");
+		int idade = sc.nextInt();	// ler um int do teclado
 		boolean passouNoTeste = false;
 		
 		if (idade >= 18 && passouNoTeste == true){
-			System.out.printf("Já pode tirar a CNH\n");
+			System.out.printf("%s, já pode tirar a CNH\n", nome);
 		} else if (idade < 18){
-			System.out.printf("É necessário ter 18 para começar\n");
+			System.out.printf("%s, é necessário ter 18 para começar\n", nome);
 		} else {
-			System.out.printf("Você deve passar no teste pscicológico\n");
+			System.out.printf("%s, você deve passar no teste pscicológico\n", nome);
 		}
 	}
 }
