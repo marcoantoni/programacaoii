19267395046a5de47c7b65ed647abf65e17a6135
Marco Antoni <marco.antoni910@gmail.com>
2023-05-09 10:08:05 -0300

    criação de métodos na classe

--- Paciente.java
+++ Paciente.java
@@ -6,32 +6,47 @@ class Paciente {
 	String prontuario;
 	String cpf;
 	
+	// criacao do método para cadastrar o paciente
+	public void cadastrarPaciente(String nome, String nascimento, String fone, String cpf){
+		// referencia this serve para resolver a ambiguidade de nomes de parametros e atributos
+		// a ambiguidade ocorre quando eles tem o MESMO nome
+		// 1º refere-se ao atributo = 2º refere-se ao parametro
+		this.nome = nome;
+		this.nascimento = nascimento;
+		this.fone = fone;
+		this.cpf = cpf;
+	}
+	
+	// método para printar os dados na tela
+	public void mostrarDados() {
+		System.out.printf("Nome: %s \n", nome);
+		System.out.printf("Nascimento: %s \n", nascimento);
+		System.out.printf("Fone: %s  CPF: %s \n", fone, cpf);
+		System.out.printf("______________________\n");
+	}
+	
 	// método principal do programa
 	public static void main (String args[]) {
 		// criacao de uma instancia da classe paciente
-		Paciente paciente1 = new Paciente();
+		Paciente paciente1 = new Paciente();	// new é o comando responsável por fazer isso
 		
-		// o paciente tem os atributos nome, nascimento, fone, cpf e prontuario
-		paciente1.nome = "Fernanda";
-		paciente1.nascimento = "20/05/2006";
-		paciente1.fone = "51 9 9812 1234";
-		paciente1.cpf = "123.456.789-10";
-		
-		// print para mostrar os atributos
-		System.out.printf("Nome: %s \n", paciente1.nome);
-		System.out.printf("Nascimento: %s \n", paciente1.nascimento);
-		System.out.printf("Fone: %s   CPF: %s \n", paciente1.fone, paciente1.cpf );
+		// chamando o método cadastrarPaciente, passando os dados como pametro
+		paciente1.cadastrarPaciente("Fernanda", "20/05/2006", "51 9 9812 1234", "123.456.789-10");
+				
+		// chamando o método mostrarDados
+		paciente1.mostrarDados();
 		
 		Paciente paciente2 = new Paciente();
 		
-		paciente2.nome = "Julius";
-		paciente2.nascimento = "06/07/2002";
-		paciente2.cpf = "987.654.321-19";
+		paciente2.cadastrarPaciente("Julius", "06/07/2002", "", "987.654.321-19");
 		
-		// print para mostrar os atributos
-		System.out.printf("Nome: %s \n", paciente2.nome);
-		System.out.printf("Nascimento: %s \n", paciente2.nascimento);
-		System.out.printf("Fone: %s   CPF: %s \n", paciente2.fone, paciente2.cpf );
+		paciente2.mostrarDados();
 		
+		Paciente paciente3 = new Paciente();
+		
+		paciente3.cadastrarPaciente("Douglas", "21/06/1990", "098.876.543-78", "(48) 9 9128 7917");
+		
+		paciente3.mostrarDados();
 	}
+	
 }
